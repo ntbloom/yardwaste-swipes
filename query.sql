0b7f49c37f3e4047b37e72d6b95c7d4fedcb57a4
ntbloom <ntbloom@gmail.com>
2022-09-01 22:25:04 -0400

    parsing by the month of august

--- query.sql
+++ query.sql
@@ -1,1 +1,11 @@
-select * from sqlite_master;
+.headers on
+
+ATTACH "databases/Db_SMS_TransHist.db" as history;
+
+SELECT 
+  TransactionHistory.transaction_date_time
+  FROM history.TransactionHistory
+  WHERE SUBSTR(TransactionHistory.transaction_date_time, 6,2) = "08";
+  ORDER BY TransactionHistory.transaction_date_time DESC
+
+;
